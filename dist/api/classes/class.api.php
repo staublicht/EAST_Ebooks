8dc72d248b86b97a4cc88ccfb9afaecd17b7ece1
Otto Edler <otto.edler@outlook.com>
2017-05-01 17:58:20 +0200

    api optimization o input gets prepared by api::input() o easier action implementation in index file

--- dist/api/classes/class.api.php
+++ dist/api/classes/class.api.php
@@ -43,6 +43,60 @@ class api
     }
 
     /**
+     * @param bool $request
+     * @param array|bool $data
+     * @return array|bool
+     */
+    public function input( $request = false, $data = false )
+    {
+
+        if( !$request || !$data )
+            return false;
+
+        $return = $this->input_get_default( $request, $this->actions, $data );
+
+        foreach( $data as $key => $value )
+            $return[$key] = $value;
+
+        return $return;
+
+    }
+
+    /**
+     * @param $request
+     * @param $structure
+     * @param $object
+     * @return array
+     */
+    private function input_get_default( $request, $structure, $object )
+    {
+
+        $return = [];
+
+        foreach( $structure as $structureKey => $structureValue )
+        {
+            foreach( $request as $requestKey => $requestValue )
+            {
+                if( $structureKey == $requestKey)
+                {
+
+                    if( $requestValue == $object )
+                        foreach( $structureValue as $key => $value )
+                            $return[$key] = false;
+
+                    if( is_object( $requestValue ) )
+                        if( !empty( $input = $this->input_get_default( $requestValue, $structureValue, $object ) ) )
+                            $return = array_merge( $return, $input);
+
+                }
+            }
+        }
+
+        return $return;
+
+    }
+
+    /**
      * Check if incomming $_POST request
      * matches whitelisted actions structure
      * @param bool $input
@@ -51,43 +105,48 @@ class api
     public function sanitize( $input = false )
     {
 
-        function whitelist( $input, $structure )
+        $result = $this->sanitize_get_whitelist( json_decode( $input ), $this->actions );
+
+        return $result;
+
+    }
+
+    /**
+     * @param $input
+     * @param $structure
+     * @return bool
+     */
+    private function sanitize_get_whitelist( $input, $structure )
+    {
+
+        $result = false;
+
+        foreach( $structure as $structureKey => $structureValue )
         {
-
-            $result = false;
-
-            foreach( $structure as $structureKey => $structureValue )
+            foreach ($input as $inputKey => $inputValue)
             {
-                foreach ($input as $inputKey => $inputValue)
+                if( $inputKey == $structureKey )
                 {
-                    if( $inputKey == $structureKey )
+
+                    if( is_object( $inputValue ) )
                     {
 
-                        if( is_object( $inputValue ) )
-                        {
-
-                            if( $inputKey == 'data' ) // keep input 'data' object alive
-                                $result->$inputKey = (array) $inputValue;
-                            else
-                                $result->$inputKey = whitelist( $inputValue, $structureValue );
-
-                        }
-
-                        else if( is_string( $inputValue ) || is_int( $inputValue ) || is_bool( $inputValue ) || is_array( $inputValue ) )
-                            $result->$inputKey = $inputValue;
-
+                        if( $inputKey == 'data' ) // keep input 'data' object alive
+                            $result->$inputKey = (array) $inputValue;
                         else
-                            $result = false;
+                            $result->$inputKey = $this->sanitize_get_whitelist( $inputValue, $structureValue );
 
                     }
+
+                    else if( is_string( $inputValue ) || is_int( $inputValue ) || is_bool( $inputValue ) || is_array( $inputValue ) )
+                        $result->$inputKey = $inputValue;
+
+                    else
+                        $result = false;
+
                 }
             }
-
-            return $result;
-
         }
-
-        $result = whitelist( json_decode( $input ), $this->actions );
 
         return $result;
 
